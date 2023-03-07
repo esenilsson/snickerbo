e7dd981837ebbafd65d5f5da8f206190988661bf
Emil Nilsson <en@one.com>
2023-03-07 20:13:30 +0100

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import logo from "$lib/assets/shed.png";
+</script>
+
+<div class="top">
+        <img alt="The project logo" src={logo} width="500px" class='logo bounce' />
+        <div class="titles bounce">
+            <h1>Snickerbo</h1>
+            <p>Like Emil's woodshed, a holding company can play a role as a place of refuge or protection for its subsidiary companies. By owning and controlling these subsidiary companies, the holding company can provide them with financial support, strategic guidance, and other resources that they may need to thrive and grow.</p>
+          <!--   <p>In some cases, a holding company may also provide legal protection for its subsidiary companies, shielding them from liability and other risks that could threaten their operations or financial stability.</p>
+            <p>Overall, the role of a holding company is to manage and support its subsidiary companies, much like Emil's woodshed provides a safe space for him to think, reflect, and ultimately improve his behavior.</p>
+  -->
+        </div>
+</div>
+
+
+<style>
+</style>
